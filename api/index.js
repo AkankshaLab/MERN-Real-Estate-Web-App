750a7cbe8927843ffa7039ca8d5818f52e916665
Akanksha52 <akanset32@gmail.com>
2024-09-27 09:09:07 +0530

    connected to database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,23 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 const app = express();
 
-app.listen(3000, ()=>{
-    console.log('Server is running on port 3000!!!');
+mongoose.connect("mongodb+srv://Akanksha:akanksha@mern-real-estate-web-ap.uwwyr.mongodb.net/mern-real-estate-web-app?retryWrites=true&w=majority&appName=mern-real-estate-web-app")
+
+    .then(() => {
+        console.log("Connected to MongoDB");
+        app.listen(3000, () => {
+            console.log('Server is running on port 3000!!!');
+        });
+    })
+    .catch((err) => {
+        console.error("Error connecting to MongoDB:", err);
+    });
+
+app.get('/test', (req, res) => {
+    res.send("Hello World");
 });
